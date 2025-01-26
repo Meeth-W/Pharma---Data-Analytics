5fc5a7bda425ffd7713d00042bebe95e4954d640
Meeth-W <meeth2111@gmail.com>
2025-01-26 21:48:12 +0530

    add main FastAPI application with static file serving and initial HTML, CSS, and JS assets

--- project/main.py
+++ project/main.py
@@ -1,1 +1,24 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Main dashboard website:
+@app.get("/")
+async def read_root():
+    return FileResponse("static/index.html")
+
+
+# API Routes:
+@app.get("/api/v1/")
+async def _():
+    return {}
+
+
+# Driver Code
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app)
